e1090f6a1a4b58ba1d4a8bc82bf759e3f472a903
9yuhyeon <kkh2742@gmail.com>
2023-07-22 20:44:24 +0900

    Feat : Entity 설계

--- src/main/java/com/sparta/abnb/entity/User.java
+++ src/main/java/com/sparta/abnb/entity/User.java
@@ -1,4 +1,58 @@
 package com.sparta.abnb.entity;
 
-public class User {
+import com.sparta.abnb.role.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column
+    private String profilePicture;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column
+    private String bio;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    // room과 연관관계
+    @OneToMany(mappedBy = "user")
+    private List<Room> rooms = new ArrayList<>();
+
+    // 예약과 연관관계
+    @OneToMany(mappedBy = "user")
+    private List<Reservation> reservations = new ArrayList<>();
+
+    // comment와 연관관계
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments = new ArrayList<>();
+
+    // wishlist와 연관관계
+    @OneToMany(mappedBy = "user")
+    private List<Wishlist> wishlists = new ArrayList<>();
+
 }
--- src/main/java/com/sparta/abnb/entity/Wishlist.java
+++ src/main/java/com/sparta/abnb/entity/Wishlist.java
@@ -1,4 +1,25 @@
 package com.sparta.abnb.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long wishlistId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="room_id")
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="user_id")
+    private User user;
+
 }
