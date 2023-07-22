5c0f129f642b63d4771627bdc49ee2cef3f98045
9yuhyeon <kkh2742@gmail.com>
2023-07-22 20:44:21 +0900

    Changes by Master

--- src/main/java/com/sparta/abnb/entity/Comment.java
+++ src/main/java/com/sparta/abnb/entity/Comment.java
@@ -1,4 +1,30 @@
 package com.sparta.abnb.entity;
 
-public class Comment {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Table(name = "comment")
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    // user와 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // room과 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
 }
