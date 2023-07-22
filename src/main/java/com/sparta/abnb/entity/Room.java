2356d5ee6f1e5caaea821fbf68695e5b502d1ee0
9yuhyeon <kkh2742@gmail.com>
2023-07-22 20:43:44 +0900

    Feat : 공통으로 사용되는 클래스 설계(Entity, JWT, ExceptionHandler, UserRole, Redis)

--- src/main/java/com/sparta/abnb/entity/Room.java
+++ src/main/java/com/sparta/abnb/entity/Room.java
@@ -1,4 +1,55 @@
 package com.sparta.abnb.entity;
 
-public class Room {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "room")
+public class Room extends Timestamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 10000)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer maxPeople;
+
+    @Column(nullable = false)
+    private String homePicture;
+
+    @Column(nullable = false)
+    private String theme;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(nullable = false)
+    private String address;
+
+    // user와 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // comment와 연관관계
+    @OneToMany(mappedBy = "room")
+    private List<Comment> comments = new ArrayList<>();
+
+    // wishlist와 연관관계
+    @OneToMany(mappedBy = "room")
+    private List<Wishlist> wishlists = new ArrayList<>();
 }
