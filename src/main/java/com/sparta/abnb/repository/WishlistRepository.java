71604a0a88ab5b266308588d07d08af406128bc6
hyun <h.zoo3963>
2023-07-24 21:29:50 +0900

    refactor : 1. UserId와 RoomId를 기준으로 좋아요의 상태 확인하는 메서드 수정 (userId를 기준 -> userId + RoomId 기준으로)

--- src/main/java/com/sparta/abnb/repository/WishlistRepository.java
+++ src/main/java/com/sparta/abnb/repository/WishlistRepository.java
@@ -4,9 +4,10 @@ import com.sparta.abnb.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
-    List<Wishlist> findByUserId(Long userId);
+    Optional<Wishlist> findByUserIdAndRoomId(Long userId, Long targetRoomId);
 
     List<Wishlist> findByUserIdOrderByCreateAtDesc(Long userId);
 
