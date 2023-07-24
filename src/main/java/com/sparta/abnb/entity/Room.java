19815fc0ba4cc96359656321cefade819bdffa11
kss <kos7114@naver.com>
2023-07-24 22:44:08 +0900

    Feat: room crud
    room entity, RoomController, RoomService crud부분 작성
    image 업로드 부분은 현재 아직 구현 되지 않았음

--- src/main/java/com/sparta/abnb/entity/Room.java
+++ src/main/java/com/sparta/abnb/entity/Room.java
@@ -1,16 +1,15 @@
 package com.sparta.abnb.entity;
 
+import com.sparta.abnb.dto.requestdto.RoomRequestDto;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @Entity
-@Builder
+//@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "room")
@@ -28,8 +27,9 @@ public class Room extends Timestamped{
     @Column(nullable = false)
     private Integer maxPeople;
 
-    @Column(nullable = false)
-    private String homePicture;
+    // room을 먼저 만들고 이후에 roompictures를 추가하면서 자연스럽게 roomPictures 부분은 채워짐
+    @OneToMany(mappedBy = "room")
+    private List<RoomPicture> roomPictures = new ArrayList<>();
 
     @Column(nullable = false)
     private String theme;
@@ -46,10 +46,30 @@ public class Room extends Timestamped{
     private User user;
 
     // comment와 연관관계
-    @OneToMany(mappedBy = "room")
+    @OneToMany(mappedBy = "room", cascade = CascadeType.REMOVE)
     private List<Comment> comments = new ArrayList<>();
 
     // wishlist와 연관관계
-    @OneToMany(mappedBy = "room")
+    @OneToMany(mappedBy = "room", cascade = CascadeType.REMOVE)
     private List<Wishlist> wishlists = new ArrayList<>();
+
+    public Room(RoomRequestDto requestDto, User user) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+        this.maxPeople = requestDto.getMaxPeople();
+        this.theme = requestDto.getTheme();
+        this.price = requestDto.getPrice();
+        this.address = requestDto.getAddress();
+
+        this.user = user;
+    }
+
+    public void update(RoomRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+        this.maxPeople = requestDto.getMaxPeople();
+        this.theme = requestDto.getTheme();
+        this.price = requestDto.getPrice();
+        this.address = requestDto.getAddress();
+    }
 }
