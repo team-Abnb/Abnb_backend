ef8ce390083d85d964174e5213161d9f0ac582a8
hyun <h.zoo3963>
2023-07-24 09:59:26 +0900

    refactor : 1. Wishlist Controller 등록 / 삭제 반환타입 수정 2. @RequestMapping url 수정 feat: Wishlist Controller 조회 생성

--- src/main/java/com/sparta/abnb/controller/WishlistController.java
+++ src/main/java/com/sparta/abnb/controller/WishlistController.java
@@ -1,28 +1,35 @@
 package com.sparta.abnb.controller;
 
 import com.sparta.abnb.dto.requestdto.WishlistRequestDto;
+import com.sparta.abnb.dto.responsedto.WishlistResponseDto;
 import com.sparta.abnb.service.WishlistService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/wishlist")
 public class WishlistController {
 
     private final WishlistService wishlistService;
 
     //좋아요 등록 / 삭제
-    @PostMapping("/wishlist")
-    public void createWishlist(
-            @RequestBody WishlistRequestDto wishlistRequestDto,
-            HttpServletRequest req )
-    {
+    @PostMapping
+    public ResponseEntity<String> createWishlist(
+            @RequestBody WishlistRequestDto wishlistRequestDto, HttpServletRequest req ) throws AccessDeniedException {
         Long targetRoomId = wishlistRequestDto.getRoomId();
-        wishlistService.createWishlist(targetRoomId,req);
+        return wishlistService.createWishlist(targetRoomId,req);
     }
 
     //좋아요 리스트 조회
+    @GetMapping
+    public List<WishlistResponseDto> getWishlists(HttpServletRequest req) throws AccessDeniedException {
+        return wishlistService.getWishlists(req);
+    }
+
 }
