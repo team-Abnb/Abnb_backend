fe094d67666168652c8fa2688bc1236f4447f4cb
hyun <h.zoo3963>
2023-07-24 10:22:43 +0900

    refactor : WishlistController / WishlistService 1. token 검증, User 조회 메소드로 따로 뽑아서 생성 2. SecurityFilter로 -> token 검증 주석처리로 수정 3. Wishlist를 생성날짜 기준으로 Desc
    feat: Wishlist 조회 생성
    body :
    1. WishlistService 에 getWishlists 메서드 생성
    2. WishlistResponseDto 생성

--- src/main/java/com/sparta/abnb/service/WishlistService.java
+++ src/main/java/com/sparta/abnb/service/WishlistService.java
@@ -1,5 +1,6 @@
 package com.sparta.abnb.service;
 
+import com.sparta.abnb.dto.responsedto.WishlistResponseDto;
 import com.sparta.abnb.entity.Room;
 import com.sparta.abnb.entity.User;
 import com.sparta.abnb.entity.Wishlist;
@@ -15,8 +16,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,27 +31,19 @@ public class WishlistService {
     private final RoomRepository roomRepository;
 
     @Transactional
-    public void createWishlist(Long targetRoomId, HttpServletRequest req){
+    //좋아요 등록 / 삭제
+    public ResponseEntity<String> createWishlist(Long targetRoomId, User user) throws AccessDeniedException {
 
-        //token 받아서 - substring - 검증
-        String token = req.getHeader(jwtUtil.HEADER_ACCESS_TOKEN);
-        String tokenValue = jwtUtil.substringToken(token);
-        if(!jwtUtil.validateAccessToken(tokenValue)){
-            throw new IllegalArgumentException("유효하지 않은 토큰입니다.");
-        }
+//        //token 받아서 - substring - 검증
+//         String tokenValue = checkToken(req);
+//        //사용자 정보 가져오기
+//        Claims info = jwtUtil.getUserInfo(tokenValue);
+//        // 가져온 사용자 정보가 UserDB에 존재하는지 확인
+//        User user = findUserMethod(info);
 
-        //사용자 정보 가져오기
-        Claims info = jwtUtil.getUserInfo(tokenValue);
-        // 가져온 사용자 정보가 먼저 UserDB에 존재하는지 확인
-        User user = userRepository.findByEmail(info.getSubject()).orElseThrow(()
-                -> new IllegalArgumentException("존재하지 않는 회원입니다."));
-
-        // userId 뽑아오기
         Long userId = user.getUserId();
-        // roomId 뽑아오기
-
         // userId에 해당하는 wishlist DB를 조회 후 userId 기준으로 roomId를 조회
-        List<Wishlist> wishlists = wishlistRepository.findByUserIn(userId);
+        List<Wishlist> wishlists = wishlistRepository.findByUserId(userId);
         Optional<Wishlist> roomIdInWishlists = wishlists.stream()
                 .filter(wishlist -> wishlist.getRoom().getRoomId().equals(targetRoomId))
                 .findFirst();
@@ -56,8 +51,7 @@ public class WishlistService {
         // 조회했을 때 targetRoomId가 있으면 좋아요 취소;
         if(roomIdInWishlists.isPresent()){
             wishlistRepository.deleteById(roomIdInWishlists.get().getWishlistId());
-            ResponseEntity.status(HttpStatus.OK).body("해당 ROOM의 좋아요를 취소하였습니다.");
-            return;
+            return ResponseEntity.status(HttpStatus.OK).body("해당 ROOM의 좋아요를 취소하였습니다.");
         }
         // 조회했을 때 targetRoomId가 없으면 좋아요 등록
         Room targetRoom = roomRepository.findById(targetRoomId).orElseThrow(()->
@@ -70,7 +64,50 @@ public class WishlistService {
 
         wishlistRepository.save(wishlist);
 
-        ResponseEntity.status(HttpStatus.OK).body("해당 ROOM의 좋아요를 등록하였습니다.");
+        return ResponseEntity.status(HttpStatus.OK).body("해당 ROOM의 좋아요를 등록하였습니다.");
 
     }
+
+    // 좋아요 리스트 조회
+    public List<WishlistResponseDto> getWishlists(User user) throws AccessDeniedException {
+
+//        //token 받아서 - substring - 검증
+//        String tokenValue = checkToken(req);
+//        Claims info = jwtUtil.getUserInfo(tokenValue);
+//
+//        // 가져온 사용자 정보가 UserDB에 존재하는지 확인
+//        // 여기서 주의할점! 썸네일 이미지를 하나 어떻게 할 것인지를 정해야함.
+//        User checkUser = findUserMethod(user);
+
+        List <Wishlist> wishlist = wishlistRepository.findByUserIdOrderByCreateAtDesc(user.getUserId());
+        List<WishlistResponseDto> wishlistResponseDtos = wishlist.stream()
+                .map(wish ->
+                        WishlistResponseDto.builder()
+                        .title(wish.getRoom().getTitle())
+                        .address(wish.getRoom().getAddress())
+                        .roomId(wish.getRoom().getRoomId())
+                        .price(wish.getRoom().getPrice())
+                        .thumbnailImg(wish.getRoom().getHomePicture())
+                        .isLike(1)
+                        .build())
+                .collect(Collectors.toList());
+
+        return wishlistResponseDtos;
+    }
+
+    // 토큰을 검증하는 메소드 <token -> substring -> 검증>
+//    private String checkToken(HttpServletRequest req) throws AccessDeniedException {
+//        String token = req.getHeader(jwtUtil.HEADER_ACCESS_TOKEN);
+//        String tokenValue = jwtUtil.substringToken(token);
+//        if (!jwtUtil.validateAccessToken(tokenValue)) {
+//            throw new AccessDeniedException("유효하지 않은 토큰입니다.");
+//        }
+//         return tokenValue;
+//    }
+
+    // user가 존재하는지 확인하는 메서드
+//    private User findUserMethod(User user) {
+//          return userRepository.findByEmail(user.getEmail()).orElseThrow(()
+//                  ->  new IllegalArgumentException("해당 USER가 존재하지 않습니다."));
+//    }
 }
