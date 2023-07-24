be5f11cf78058d0a722e3d2482f3fbc60c2081a9
hyun <h.zoo3963>
2023-07-24 15:10:56 +0900

    feat : User 프로필 조회 생성

--- src/main/java/com/sparta/abnb/controller/UserController.java
+++ src/main/java/com/sparta/abnb/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.sparta.abnb.controller;
 
+import com.sparta.abnb.dto.responsedto.UserResponseDto;
+import com.sparta.abnb.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/users/profile")
 public class UserController {
+    private final UserService userService;
+
+    //프로필 조회는 누구나 가능하게
+    @GetMapping("/{userId}")
+    public UserResponseDto getUserProfile(@PathVariable Long userId){
+        return userService.getUserProfile(userId);
+    }
+
 }
--- src/main/java/com/sparta/abnb/service/UserService.java
+++ src/main/java/com/sparta/abnb/service/UserService.java
@@ -1,4 +1,26 @@
 package com.sparta.abnb.service;
 
+import com.sparta.abnb.dto.responsedto.UserResponseDto;
+import com.sparta.abnb.entity.User;
+import com.sparta.abnb.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserResponseDto getUserProfile(Long userId) {
+        User checkUser =  userRepository.findById(userId).orElseThrow(()
+                -> new IllegalArgumentException("해당 USER가 존재하지 않습니다."));
+
+        return UserResponseDto.builder()
+                .profileImageUrl(checkUser.getProfilePicture())
+                .username(checkUser.getUsername())
+                .bio(checkUser.getBio())
+                .build();
+    }
+
 }
