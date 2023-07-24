6d7f8aeb7c46b1ac2a59cb26f4fa6e9d01dd25f7
hyun <h.zoo3963>
2023-07-24 11:13:00 +0900

    feat : Comment 등록 기능 생성 body : 1. Controller 추가 2. 연관된 Repository 수정(Comment/Room) 3. RequestDto/ResponseDto 생성

---
+++ src/main/java/com/sparta/abnb/dto/responsedto/CommentResponseDto.java
@@ -0,0 +1,12 @@
+package com.sparta.abnb.dto.responsedto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentResponseDto {
+    private Long commentId;
+    private String comment;
+}
