58a5088f7c3d5f479069e031298138f9bf111b3c
hyun <h.zoo3963>
2023-07-25 14:17:26 +0900

    refactor : 비밀번호 변경, 프로필 이미지 업데이트 수정

--- src/main/java/com/sparta/abnb/service/MypageService.java
+++ src/main/java/com/sparta/abnb/service/MypageService.java
@@ -1,13 +1,12 @@
 package com.sparta.abnb.service;
 
-import com.sparta.abnb.dto.requestdto.UserRequestDto;
+import com.sparta.abnb.dto.requestdto.MypageRequestDto;
 import com.sparta.abnb.dto.responsedto.MypageResponseDto;
-import com.sparta.abnb.dto.responsedto.UserResponseDto;
-import com.sparta.abnb.entity.Mypage;
 import com.sparta.abnb.entity.User;
-import com.sparta.abnb.repository.MypageRepository;
 import com.sparta.abnb.repository.UserRepository;
+import com.sparta.abnb.util.S3Util;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,15 +15,16 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class MypageService {
 
-    private final MypageRepository mypageRepository;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final S3Util s3Util;
 
     //마이페이지 조회
     public MypageResponseDto getUserProfile(Long userId) {
         User checkuser =  userRepository.findById(userId).orElseThrow(()
                 -> new IllegalArgumentException("해당 USER가 존재하지 않습니다."));
 
-        Mypage mypage = checkuser.getMypage();
+
         return MypageResponseDto.builder()
                 .profileImageUrl(checkuser.getProfilePicture())
                 .username(checkuser.getUsername())
@@ -33,9 +33,9 @@ public class MypageService {
     }
     // 프로필 수정
     @Transactional
-    public MypageResponseDto updateUserProfile(Long userId, User user, MultipartFile file, UserRequestDto userRequestDto) {
+    public MypageResponseDto updateUserProfile(Long userId, User user, MultipartFile file, MypageRequestDto mypageRequestDto) {
 
-        if(!user.getUserId().equals(userId)){
+        if (!user.getUserId().equals(userId)) {
             throw new IllegalArgumentException("해당 USER가 아닙니다.");
         }
 
@@ -43,24 +43,26 @@ public class MypageService {
         // 웹에서 현재 비번을 확인 - 새 비밀번호를 적는게 한 공간에 있기 때문에 새로운 비번이 적혀있다면?
         // 비밀번호를 수정한다는 뜻으로 받아드리고 이 로직을 작성함.
         String newPassword = null;
-        if(userRequestDto.getNewPassword() != null){
-            if(!user.getPassword().equals(userRequestDto.getPassword())) {
+        if (mypageRequestDto.getPassword() != null) {
+            if (!user.getPassword().equals(mypageRequestDto.getPassword())) {
                 throw new IllegalArgumentException("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
             }
-            newPassword = userRequestDto.getNewPassword();
+            newPassword = passwordEncoder.encode(mypageRequestDto.getNewPassword());
         }
 
         //이미지 수정 로직
-        if(file != null){
+        String url = null;
+        if (file != null) {
             // 기본이미지를 보내주거나? 새로운 이미지르 보내주거나
+            url = s3Util.updateImage(user.getProfilePicture(), file, "profilePicture");
         }
         // entity 저장
         User updateUser = User.builder()
-                .username(userRequestDto.getUsername())
-                .phoneNumber(userRequestDto.getPhoneNumber())
-                .bio(userRequestDto.getBio())
-                .profilePicture(user.getProfilePicture())
-                .password(newPassword)
+                .username(mypageRequestDto.getUsername())
+                .phoneNumber(mypageRequestDto.getPhoneNumber())
+                .bio(mypageRequestDto.getBio())
+                .profilePicture(file == null ? user.getProfilePicture() : url)
+                .password(mypageRequestDto.getPassword() != null ? newPassword : user.getPassword())
                 .build();
 
         userRepository.save(updateUser);
